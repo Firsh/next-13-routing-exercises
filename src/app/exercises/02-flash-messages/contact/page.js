5fbd4b849aa2b644326e2912068ba148913b0f52
Firsh <firshee@gmail.com>
2024-02-03 15:06:30 +0000

    allegedly better solution :D

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -2,14 +2,17 @@
 import React from 'react';
 
 import { useRouter } from 'next/navigation';
+import { ToastContext } from '@/c/ToastProvider';
 
 
 function ContactPage() {
+  const { createToast } = React.useContext(ToastContext);
   const router = useRouter();
   function handleSubmit(event) {
     event.preventDefault();
     console.log("handling submit");
-    router.push('/exercises/02-flash-messages?from=contact');
+    router.push('/exercises/02-flash-messages');
+    createToast("Message sent!", "success");
   }
   return (
     <main>
--- src/app/exercises/02-flash-messages/page.js
+++ src/app/exercises/02-flash-messages/page.js
@@ -1,36 +1,24 @@
 'use client';
 import React from 'react';
 import Link from 'next/link';
-import Toast from '@/c/Toast';
-import { createPortal } from 'react-dom';
 import ToastShelf from '@/c/ToastShelf';
-import { ToastContext } from '@/c/ToastProvider';
-
 function FlashMsgExercise({ params, searchParams }) {
-  const { createToast, toasts } = React.useContext(ToastContext);
-  const mess = "Message sent!";
-  React.useEffect(() => {
-    if (searchParams.from === 'contact') {
-      if (toasts.length !== 0) {
-        if (toasts[0].message !== mess) {
-        }
-      } else {
-        createToast(mess, "success");
-      }
-    }
-  }, []);
+
   return (
-    <main>
-      <h1>Welcome to my website!</h1>
-      <p>
-        Got feedback? Please{' '}
-        <Link href="/exercises/02-flash-messages/contact">
-          contact us
-        </Link>
-        .
-      </p>
+    <>
+      <main>
+        <h1>Welcome to my website!</h1>
+        <p>
+          Got feedback? Please{' '}
+          <Link href="/exercises/02-flash-messages/contact">
+            contact us
+          </Link>
+          .
+        </p>
+
+      </main>
       <ToastShelf />
-    </main>
+    </>
   );
 }
 
